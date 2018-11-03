accdfe7a4da821e0bf3d794b1a5128d0ccb5cf30
北添真広 <sas.surfer0jonny@gmail.com>
2018-11-03 23:32:24 +0900

    apply bootstrap template

--- cam_ranks/urls.py
+++ cam_ranks/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from search import views as search_views
 
 urlpatterns = [
+    path('', include('search.urls')),
     path('ranking/', include('ranking.urls')),
-    path('search/', include('search.urls')),
     path('admin/', admin.site.urls),
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def top_page(request):
+  return render(request, 'search/top_page.html', {})
+
+def index(request):
+  return render(request, 'search/index.html', {})
