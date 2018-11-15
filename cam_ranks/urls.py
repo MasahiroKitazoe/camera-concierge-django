5345d65eb5b2c14cda3ff3eb8391183265603ac8
北添真広 <sas.surfer0jonny@gmail.com>
2018-11-15 21:57:10 +0900

    add ranking view and model

--- cam_ranks/urls.py
+++ cam_ranks/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from search import views as search_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('search.urls')),
     path('ranking/', include('ranking.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
