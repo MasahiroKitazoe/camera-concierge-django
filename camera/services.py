91698a67ca6468c0c578a49d174d9baec3e85a99
MasahiroKitazoe <sas.surfer0jonny@gmail.com>
2018-12-12 21:05:51 +0900

    インデント整える

--- camera/services.py
+++ camera/services.py
@@ -5,19 +5,19 @@ from ranking.models import Rank
 
 
 class CameraSearcher:
-  def filter_cameras_by_ranking(self, rank_id):
-    rank = Rank.objects.get(pk=rank_id)
-    rank_dict = rank.__dict__
+    def filter_cameras_by_ranking(self, rank_id):
+        rank = Rank.objects.get(pk=rank_id)
+        rank_dict = rank.__dict__
 
-    # 絞り込み条件のないフィールドを弾く
-    criteria_dict = {}
-    for key, val in criteria_dict.items():
-      if val == 0 or val is None:
-        continue
-      criteria_dict[key] = val
+        # 絞り込み条件のないフィールドを弾く
+        criteria_dict = {}
+        for key, val in criteria_dict.items():
+          if val == 0 or val is None:
+            continue
+          criteria_dict[key] = val
 
-    for key, val in criteria_dict.items():
-      pass
+        for key, val in criteria_dict.items():
+        pass
 
-  def filter_cameras_by_users(self):
-    pass
+    def filter_cameras_by_users(self):
+        pass
