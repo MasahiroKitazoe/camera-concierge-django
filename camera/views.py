5a3c755cf9b69c4eaa90e88f3f102c9e6fda6f97
MasahiroKitazoe <sas.surfer0jonny@gmail.com>
2018-12-14 22:00:23 +0900

    カメラを検索するメソッドを実装

--- camera/views.py
+++ camera/views.py
@@ -3,16 +3,18 @@ from django.shortcuts import render
 from .forms import SearchForm
 from .services import CameraSearcher
 
+
 def top_page(request):
-  return render(request, 'camera/top_page.html', {})
+    return render(request, 'camera/top_page.html', {})
+
 
 def search(request):
-  if len(request.GET) == 0:
-    form = SearchForm()
-    return render(request, 'camera/search.html', {"form": form})
+    if len(request.GET) == 0:
+        form = SearchForm()
+        return render(request, 'camera/search.html', {"form": form})
 
-  form = SearchForm(request.GET)
-  form.is_valid()  # エラーは起きない想定
+    form = SearchForm(request.GET)
+    form.is_valid()  # エラーは起きない想定
 
-  cameras = CameraSearcher.filter(form.cleaned_data)
-  return render(request, 'camera/search.html', {"form": form, "cameras": cameras})
+    cameras = CameraSearcher.filter(form.cleaned_data)
+    return render(request, 'camera/search.html', {"form": form, "cameras": cameras})
