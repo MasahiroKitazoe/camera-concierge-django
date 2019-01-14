2b03f50e7911f4f73150f5032783230cc127f798
北添真広 <sas.surfer0jonny@gmail.com>
2019-01-14 19:00:11 +0900

    update import method

--- ranking/utils.py
+++ ranking/utils.py
@@ -4,7 +4,7 @@ import math
 def assign_item(item):
     """csvからのデータインポートの時に、空白値や不要表記を整形して保存可能にする"""
     if type(item) == float and math.isnan(item):
-        return None
+        return 0
     else:
         if type(item) == str:
             item = item.replace("\u3000", " ")
