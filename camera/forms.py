4efe2b3231bcf0036940a0fe27a5bb36d1d1f8b7
北添真広 <sas.surfer0jonny@gmail.com>
2019-01-14 18:38:17 +0900

    prepare for migrations

--- camera/forms.py
+++ camera/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from .models import Camera
-from .consts import SHUTTER_SPEED, F_VALUE, BOOLEAN, CAMERA_TYPE_CHOICES, FINDER_CHOICES, FRAME_CHOICES, MAKER_CHOICES
+# from .consts import SHUTTER_SPEED, F_VALUE, BOOLEAN, CAMERA_TYPE_CHOICES, FINDER_CHOICES, FRAME_CHOICES, MAKER_CHOICES
 
 
 class SearchForm(ModelForm):
@@ -14,8 +14,8 @@ class SearchForm(ModelForm):
       forms.FloatField(label="連写性能(下限)", required=False)
     max_continuous_shooting_performance = \
       forms.FloatField(label="連写性能(上限)", required=False)
-    min_shutter_speed = forms.ChoiceField(label="シャッタースピード(下限)", required=False, choices=SHUTTER_SPEED)
-    max_shutter_speed = forms.ChoiceField(label="シャッタースピード(上限)", required=False, choices=SHUTTER_SPEED)
+    # min_shutter_speed = forms.ChoiceField(label="シャッタースピード(下限)", required=False, choices=SHUTTER_SPEED)
+    # max_shutter_speed = forms.ChoiceField(label="シャッタースピード(上限)", required=False, choices=SHUTTER_SPEED)
     min_monitor_size = forms.FloatField(label="モニターサイズ(下限)", required=False)
     max_monitor_size = forms.FloatField(label="モニターサイズ(上限)", required=False)
     min_monitor_pixel = forms.IntegerField(label="モニター解像度(下限)", required=False)
@@ -30,10 +30,10 @@ class SearchForm(ModelForm):
     max_height = forms.FloatField(label="高さ(上限)", required=False)
     min_depth = forms.FloatField(label="奥行(下限)", required=False)
     max_depth = forms.FloatField(label="奥行(上限)", required=False)
-    min_f_value_wide = forms.ChoiceField(label="ワイド端F値(下限)", required=False, choices=F_VALUE)
-    max_f_value_wide = forms.ChoiceField(label="ワイド端F値(上限)", required=False, choices=F_VALUE)
-    min_f_value_tele = forms.ChoiceField(label="テレ端F値(下限)", required=False, choices=F_VALUE)
-    max_f_value_tele = forms.ChoiceField(label="テレ端F値(上限)", required=False, choices=F_VALUE)
+    # min_f_value_wide = forms.ChoiceField(label="ワイド端F値(下限)", required=False, choices=F_VALUE)
+    # max_f_value_wide = forms.ChoiceField(label="ワイド端F値(上限)", required=False, choices=F_VALUE)
+    # min_f_value_tele = forms.ChoiceField(label="テレ端F値(下限)", required=False, choices=F_VALUE)
+    # max_f_value_tele = forms.ChoiceField(label="テレ端F値(上限)", required=False, choices=F_VALUE)
     min_shooting_num_with_finder = \
       forms.IntegerField(label="ファインダー使用時の撮影可能枚数(下限)", required=False)
     max_shooting_num_with_finder = \
@@ -48,21 +48,21 @@ class SearchForm(ModelForm):
       forms.FloatField(label="マクロモード時最短撮影距離(上限)", required=False)
     min_open_date = forms.DateField(label="発売日(下限)", required=False)
     max_open_date = forms.DateField(label="発売日(上限)", required=False)
-    four_k = forms.ChoiceField(label="4K動画撮影", required=False, choices=BOOLEAN)
-    wifi = forms.ChoiceField(label="Wifi機能", required=False, choices=BOOLEAN)
-    touch_panel = forms.ChoiceField(label="タッチパネル", required=False, choices=BOOLEAN)
-    move_panel = forms.ChoiceField(label="可動式モニター", required=False, choices=BOOLEAN)
-    bluetooth = forms.ChoiceField(label="Bluetooth", required=False, choices=BOOLEAN)
-    selfie = forms.ChoiceField(label="自撮り", required=False, choices=BOOLEAN)
-    waterploof = forms.ChoiceField(label="防水", required=False, choices=BOOLEAN)
-    gps = forms.ChoiceField(label="GPS", required=False, choices=BOOLEAN)
-    anti_shake = forms.ChoiceField(label="手ぶれ補正", required=False, choices=BOOLEAN)
-    five_axis_anti_shake = forms.ChoiceField(label="ボディ内5軸手ぶれ補正", required=False, choices=BOOLEAN)
-    super_wide = forms.ChoiceField(label="超広角", required=False, choices=BOOLEAN)
-    camera_type_id = forms.ChoiceField(label="カメラタイプ", required=False, choices=CAMERA_TYPE_CHOICES)
-    frame_id = forms.ChoiceField(label="センサーサイズ", required=False, choices=FRAME_CHOICES)
-    finder_id = forms.ChoiceField(label="ファインダー", required=False, choices=FINDER_CHOICES)
-    maker_id = forms.ChoiceField(label="メーカー", required=False, choices=MAKER_CHOICES)
+    # four_k = forms.ChoiceField(label="4K動画撮影", required=False, choices=BOOLEAN)
+    # wifi = forms.ChoiceField(label="Wifi機能", required=False, choices=BOOLEAN)
+    # touch_panel = forms.ChoiceField(label="タッチパネル", required=False, choices=BOOLEAN)
+    # move_panel = forms.ChoiceField(label="可動式モニター", required=False, choices=BOOLEAN)
+    # bluetooth = forms.ChoiceField(label="Bluetooth", required=False, choices=BOOLEAN)
+    # selfie = forms.ChoiceField(label="自撮り", required=False, choices=BOOLEAN)
+    # waterploof = forms.ChoiceField(label="防水", required=False, choices=BOOLEAN)
+    # gps = forms.ChoiceField(label="GPS", required=False, choices=BOOLEAN)
+    # anti_shake = forms.ChoiceField(label="手ぶれ補正", required=False, choices=BOOLEAN)
+    # five_axis_anti_shake = forms.ChoiceField(label="ボディ内5軸手ぶれ補正", required=False, choices=BOOLEAN)
+    # super_wide = forms.ChoiceField(label="超広角", required=False, choices=BOOLEAN)
+    # camera_type_id = forms.ChoiceField(label="カメラタイプ", required=False, choices=CAMERA_TYPE_CHOICES)
+    # frame_id = forms.ChoiceField(label="センサーサイズ", required=False, choices=FRAME_CHOICES)
+    # finder_id = forms.ChoiceField(label="ファインダー", required=False, choices=FINDER_CHOICES)
+    # maker_id = forms.ChoiceField(label="メーカー", required=False, choices=MAKER_CHOICES)
 
     class Meta:
         model = Camera
