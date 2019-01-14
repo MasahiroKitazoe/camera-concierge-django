b0cb5e58cced54892d42282670cd3461535406ab
北添真広 <sas.surfer0jonny@gmail.com>
2019-01-14 12:35:40 +0900

    config for db connection

--- cam_ranks/settings/production.py
+++ cam_ranks/settings/production.py
@@ -1,15 +1,24 @@
 from .base import *
 
+DATABASES = env("DATABASES")
+HOST = env("HOST")
+DB_PASSWORD = env("DB_PASSWORD")
+DB_USER = env("DB_USER")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cam_ranks_production',
+        "DATABASES": DATABASES
+        "HOST": HOST,
+        "PASSWORD": DB_PASSWORD,
+        "USER": DB_USER,
+        "PORT": "5432",
     }
 }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=400)
 DATABASES['default'].update(db_from_env)
-
 
 DEBUG = False
