9defd388c133d1c6e0acd8745ba2a4f7c460b75e
MasahiroKitazoe <sas.surfer0jonny@gmail.com>
2019-02-02 14:14:26 +0900

    activate only on Saturday

--- crawler/management/commands/scrape_insta_hashtags.py
+++ crawler/management/commands/scrape_insta_hashtags.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.core.management.base import BaseCommand
 
 from crawler.scraper import HashTagScraper
@@ -5,8 +6,9 @@ from camera.models import HashTag
 
 
 class Command(BaseCommand):
-    help = "カメラのインスタハッシュタグ数を取得し、csvに出力する"
+    help = "カメラのインスタハッシュタグ数を取得し、csvに出力する。毎週土曜日だけの定期実行"
 
     def handle(self, *args, **options):
-        HashTagScraper.scrape_insta_hashtags()
-        HashTag.import_csv("crawler/output/posted_count_insta.csv")
+        if timezone.localdate().weekday() == 5:
+            HashTagScraper.scrape_insta_hashtags()
+            HashTag.import_csv("crawler/output/posted_count_insta.csv")
